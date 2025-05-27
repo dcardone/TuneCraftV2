49aa22eb98696211560cee279ee80656443f3e1b
Dominick Cardone <cardoned11@gmail.com>
2025-05-27 14:26:13 -0400

    added error alert on bad response

--- craft.js
+++ craft.js
@@ -6,10 +6,12 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config({
-  path: path.resolve(__dirname, "credentialsDontPost/.env"),
+  path: path.resolve(__dirname, "credentials/.env"),
 });
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(__dirname + "/public"));
 
 const PORT = process.env.PORT || 10000;
@@ -19,6 +21,7 @@ let userId;
 let database;
 let collection;
 let client;
+
 const databaseName = "TuneCraftDB";
 const collectionName = "UserLogs";
 
@@ -50,24 +53,24 @@ async function putInDatabase(record) {
 
 /* INITIALIZE SEED SONGS */
 seed_songs = new Map();
-seed_songs.set("sad pop", "7qEHsqek33rTcFNT9PFqLf"); // Someone you loved; Lewis Capaldi
-seed_songs.set("neutral pop", "3pHkh7d0lzM2AldUtz2x37"); // The Archer; Taylor Swift
-seed_songs.set("happy pop", "4kbj5MwxO1bq9wjT5g9HaA"); // Shut up and dance; Walk the Moon
-seed_songs.set("sad classic", "7nHvS6UUhz2gJhj8TIROLX"); // Adagio for Strings; Samuel Barber
-seed_songs.set("neutral classic", "6kf7ZCJjEbjZXikivKOsvJ"); // Claire de Lune; Claude Debussy
-seed_songs.set("happy classic", "5rk76Ugo6ZWsciJwvCQ4vH"); // Spring; Vivaldi
-seed_songs.set("sad rock", "5Xak5fmy089t0FYmh3VJiY"); // Black; Pearl Jam
-seed_songs.set("neutral rock", "5UWwZ5lm5PKu6eKsHAGxOk"); // Everlong; Foo Fighters
-seed_songs.set("happy rock", "65OR4ywy8Cgs3FDHK82Idl"); // Higher Power; Cold Play
-seed_songs.set("sad rap", "561jH07mF1jHuk7KlaeF0s"); // Mockingbird; Eminem
-seed_songs.set("neutral rap", "7KXjTSCq5nL1LoYtL7XAwS"); // HUMBLE; Kendrick Lamar
-seed_songs.set("happy rap", "4Nv8GzZWyZwjo08aoxDTv9"); // When We Party; Faith Evans (feat. Snoop Dogg)
-seed_songs.set("sad jazz", "1wl5b2lw3YagQtZiYZbQWP"); // 'Round Midnight; Thelonious Monk
-seed_songs.set("neutral jazz", "4vLYewWIvqHfKtJDk8c8tq"); // So What; Miles Davis
-seed_songs.set("happy jazz", "5L8ta4ECl5zeA6bGqY7G38"); // Sing, sing, sing; Benny Goodman
-seed_songs.set("sad funk", "2uMqG8w8oi09fB2YA2QLOm"); // Summer Madness; Kool & The Gang
-seed_songs.set("neutral funk", "2x1LQq8lsUzAA2wNj8yjC9"); // Pick Up the Pieces; Average White Band
-seed_songs.set("happy funk", "5XeSAezNDk9tuw3viiCbZ3"); // Get Up Offa That Thing; James Brown
+seed_songs.set("sad pop", "7qEHsqek33rTcFNT9PFqLf");          // Someone you loved; Lewis Capaldi
+seed_songs.set("neutral pop", "3pHkh7d0lzM2AldUtz2x37");      // The Archer; Taylor Swift
+seed_songs.set("happy pop", "4kbj5MwxO1bq9wjT5g9HaA");        // Shut up and dance; Walk the Moon
+seed_songs.set("sad classic", "7nHvS6UUhz2gJhj8TIROLX");      // Adagio for Strings; Samuel Barber
+seed_songs.set("neutral classic", "6kf7ZCJjEbjZXikivKOsvJ");  // Claire de Lune; Claude Debussy
+seed_songs.set("happy classic", "5rk76Ugo6ZWsciJwvCQ4vH");    // Spring; Vivaldi
+seed_songs.set("sad rock", "5Xak5fmy089t0FYmh3VJiY");         // Black; Pearl Jam
+seed_songs.set("neutral rock", "5UWwZ5lm5PKu6eKsHAGxOk");     // Everlong; Foo Fighters
+seed_songs.set("happy rock", "65OR4ywy8Cgs3FDHK82Idl");       // Higher Power; Cold Play
+seed_songs.set("sad rap", "561jH07mF1jHuk7KlaeF0s");          // Mockingbird; Eminem
+seed_songs.set("neutral rap", "7KXjTSCq5nL1LoYtL7XAwS");      // HUMBLE; Kendrick Lamar
+seed_songs.set("happy rap", "4Nv8GzZWyZwjo08aoxDTv9");        // When We Party; Faith Evans (feat. Snoop Dogg)
+seed_songs.set("sad jazz", "1wl5b2lw3YagQtZiYZbQWP");         // 'Round Midnight; Thelonious Monk
+seed_songs.set("neutral jazz", "4vLYewWIvqHfKtJDk8c8tq");     // So What; Miles Davis
+seed_songs.set("happy jazz", "5L8ta4ECl5zeA6bGqY7G38");       // Sing, sing, sing; Benny Goodman
+seed_songs.set("sad funk", "2uMqG8w8oi09fB2YA2QLOm");         // Summer Madness; Kool & The Gang
+seed_songs.set("neutral funk", "2x1LQq8lsUzAA2wNj8yjC9");     // Pick Up the Pieces; Average White Band
+seed_songs.set("happy funk", "5XeSAezNDk9tuw3viiCbZ3");       // Get Up Offa That Thing; James Brown
 
 user = "";
 
@@ -83,9 +86,8 @@ app.get("/selectMood", (req, res) => {
 });
 
 app.post("/suggestions", (req, res) => {
-  /************************/
-  //Random Dominick stuff
-  /************************/
+
+  /* CATEGORIZE MOOD BASED ON SLIDER */
   let mood = "";
   if (req.body.valence >= 0.66) {
     mood = "happy";
@@ -94,15 +96,13 @@ app.post("/suggestions", (req, res) => {
   } else {
     mood = "sad";
   }
-  /************************/
-  //Random Dominick stuff p2
-  /************************/
+
+  /* COMBINE MOOD WITH GENRE TO GET SEED SONG */
   const song_type = mood + " " + req.body.genre;
   const seed_song = seed_songs.get(song_type);
   console.log(song_type);
-  /************************/
-  //Random Dominick stuff p3
-  /************************/
+
+  /* CONSTRUCT REQUEST */
   const config = {
     method: "get",
     maxBodyLength: Infinity,
@@ -111,20 +111,21 @@ app.post("/suggestions", (req, res) => {
       Accept: "application/json",
     },
   };
-  /************************/
-  //Random Dominick stuff p4
-  /************************/
+
+  /* SEND REQUEST */
   axios
     .request(config)
     .then((response) => {
+      /*******************************************************************
+      RECCOBEATS API OFTEN PROVIDES SONGS WITH VERY FEW LISTENS.
+      SO, WE REQUEST 100 SONGS, SORT BY POPULARITY, AND CHOOSE RANDOMLY
+      AMONG THE TOP 3 MOST POPULAR SONGS TO RECOMMEND 
+      *******************************************************************/
       const songs = response.data.content;
       songs.sort((a, b) => b.popularity - a.popularity);
       const songRecs = songs.slice(0, 3);
-      //console.log(songRecs);
 
-      /************************/
-      //Declaring variables to create table and clean up data
-      /************************/
+      /* CREATE TABLE */
       let reccomendationsTable = `
       <table border="1">
         <tr>
@@ -135,16 +136,14 @@ app.post("/suggestions", (req, res) => {
       let cleanedSongRecs = Array(songRecs.length);
       let i = 0;
 
-      /************************/
-      //Creating the table and cleaning up the data pt2
-      /************************/
+      /* FILL IN TABLE WITH SONG RECS */
       for (const song of songRecs) {
         let Artists = song.artists
           .reduce((acc, e) => {
             return acc + e.name + ", ";
           }, "")
           .trim()
-          .slice(0, -1); //Remove space and get rid of extra comma
+          .slice(0, -1); // Remove space and get rid of extra comma
         cleanedSongRecs[i++] = {
           song: song.trackTitle,
           artists: Artists,
@@ -154,9 +153,7 @@ app.post("/suggestions", (req, res) => {
       }
       reccomendationsTable += `</table>`;
 
-      /************************/
-      //Creating object to store and pushing to Mongo
-      /************************/
+     /* CREATING OBJECT TO STORE IN DB */
       let toLog = {
         userId: userId,
         songRecs: cleanedSongRecs,
@@ -164,16 +161,11 @@ app.post("/suggestions", (req, res) => {
       };
       putInDatabase(toLog);
 
-      /************************/
-      //Render Template
-      /************************/
+      /* RENDER TEMPLATE */
       res.render("suggestions", { reccomendationsTable });
     })
     .catch((error) => {
-      console.log(error);
-      res
-        .status(500)
-        .send("An error occurred while fetching song suggestions.");
+      res.render("selectMood", {error: "Sorry, no songs matched that set of moods. You must be feeling wild! Try again with another combination of values!"});
     });
 });
 app.get("/history", async (req, res) => {
